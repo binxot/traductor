b44ab9d54de65c9ca43f1200c48af2229d0d2a20
binxot <frvagu@gmail.com>
2022-10-27 09:23:32 +0200

    new file:   altavoz512.png 	modified:   index.html 	new file:   microphone-solid-24.png 	modified:   traductor.css 	modified:   traductor.js

--- traductor.js
+++ traductor.js
@@ -1,3 +1,5 @@
+var idiomas = ["es","en","fr","de","it","pt","ru","ja","zh"];
+
 function translateText(){
     var text = document.getElementById("entrada").value;
     var idiomaOrigen = document.getElementById("selectIdiomaOrigen").value;
@@ -24,3 +26,41 @@ function translateText(){
 document.addEventListener('change', function() {
     translateText();
 });
+
+//Microfono
+function escuchar(){
+    var recognition = new (webkitSpeechRecognition || SpeechRecognition)();
+    recognition.lang = "es";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+    recognition.start();
+
+    /* Para ver los eventos en la consola
+    ['onaudiostart','onaudioend','onend','onerror','onnomatch','onresult','onsoundstart','onsoundend','onspeechend','onstart']
+    .forEach(function(eventName) {
+        recognition[eventName] = function(e) {
+            console.log(eventName, e);
+        };
+    });*/
+
+    recognition.onresult = function() {
+        document.getElementById("entrada").value = event.results[0][0].transcript;
+    };
+}
+
+//voces
+speechSynthesis.addEventListener("voiceschanged", function() {
+    var voices = speechSynthesis.getVoices();
+    for(var i = 0; i < voices.length ; i++) {
+      document.getElementById("speech-select").innerHTML += '<option value="' + voices[i].name + '">(' + voices[i].lang + ')</option>';
+    }
+});
+
+//Altavoz
+function hablar(){
+    var text = document.getElementById("salida").textContent;
+    var voice = document.getElementById("speech-select").value;
+    var utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == document.getElementById("speech-select").value; })[0];
+    speechSynthesis.speak(utterance);
+}
